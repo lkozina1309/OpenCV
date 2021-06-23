20b3b7f1011bf6680e91b74161266ca8a9c77397
lkozina1309 <65762882+lkozina1309@users.noreply.github.com>
2021-06-23 16:10:26 +0200

    Update mouse_click.py

--- mouse_click.py
+++ mouse_click.py
@@ -1,4 +1,4 @@
-# Script mouse_click.py shows location on the image.
+# Script mouse_click.py shows location on the image. It uses image of a footballer Mason Mount so You need to download an image and provide the right path to it.
 
 import numpy as np
 import cv2
@@ -18,7 +18,7 @@ def click_event(event, x, y, flags, param):
 		cv2.putText(img, strBGR, (x,y), font, .5, (0,255,255), 2)
 		cv2.imshow('image', img)
 
-img = cv2.imread('/home/marija/opencv/samples/data/mount.jpeg', 1)
+img = cv2.imread('mount.jpeg', 1)
 img = cv2.resize(img, (512,512))
 cv2.imshow('image',img)
 
