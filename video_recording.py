5b46b44ffc3a82d674198b65e30c217585c3a37e
lkozina1309 <65762882+lkozina1309@users.noreply.github.com>
2021-06-23 09:49:40 +0200

    Create video_recording.py

---
+++ video_recording.py
@@ -0,0 +1,24 @@
+# Script video_recording.py uses webcam to record a video.
+
+import cv2
+
+cap = cv2.VideoCapture(0);
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('snimak.avi', fourcc, 20.0, (640,480))
+
+
+while(cap.isOpened()):
+	ret, frame = cap.read()
+	if ret == True:
+		
+		out.write(frame)
+		cv2.imshow('frame', frame)
+		
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+	else:
+		break
+		
+cap.release()
+out.release()
+cv2.destroyAllWindows()
