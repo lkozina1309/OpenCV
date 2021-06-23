fd2f8c48cf5295e709789dd1f8ab3e69c49c788a
lkozina1309 <65762882+lkozina1309@users.noreply.github.com>
2021-06-23 16:39:56 +0200

    Update adaptive_threshold.py

--- adaptive_threshold.py
+++ adaptive_threshold.py
@@ -4,7 +4,7 @@
 import numpy as np
 import cv2 as cv
 
-img = cv.imread('/home/marija/opencv/samples/data/sudoku.png', 0)
+img = cv.imread('sudoku.png', 0)
 #_th1 = cv.treshold(img, 127, 255, cv.THRESH_BINARY)
 th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2);
 th3 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2);
