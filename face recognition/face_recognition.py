6d9c964d5da99037484b09aa05ac4b8c13e299e3
lkozina1309 <65762882+lkozina1309@users.noreply.github.com>
2021-06-24 14:28:45 +0200

    Update face_recognition.py

--- face recognition/face_recognition.py
+++ face recognition/face_recognition.py
@@ -1,4 +1,5 @@
-# Script face_recognition.py 
+# Script face_recognition.py uses data library with 30 faces to find whose face matches to test.jpg.
+# You can change the test.jpg image and check if the face is recognized. You can also put some new faces in data library
 
 
 import face_recognition as fr
@@ -13,7 +14,7 @@ def get_encoded_faces():
    
     encoded = {}
 
-    for dirpath, dnames, fnames in os.walk("./faces"):
+    for dirpath, dnames, fnames in os.walk("./data"):
         for f in fnames:
             if f.endswith(".jpg") or f.endswith(".png") or f.endswith(".jpeg"):
                 face = fr.load_image_file("faces/" + f)
@@ -24,9 +25,7 @@ def get_encoded_faces():
 
 
 def unknown_image_encoded(img):
-    """
-    encode a face given the file name
-    """
+   
     face = fr.load_image_file("faces/" + img)
     encoding = fr.face_encodings(face)[0]
 
@@ -34,31 +33,23 @@ def unknown_image_encoded(img):
 
 
 def classify_face(im):
-    """
-    will find all of the faces in a given image and label
-    them if it knows what they are
-
-    :param im: str of file path
-    :return: list of face names
-    """
+   
     faces = get_encoded_faces()
     faces_encoded = list(faces.values())
     known_face_names = list(faces.keys())
 
     img = cv2.imread(im, 1)
-    #img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-    #img = img[:,:,::-1]
- 
+     
     face_locations = face_recognition.face_locations(img)
     unknown_face_encodings = face_recognition.face_encodings(img, face_locations)
 
     face_names = []
     for face_encoding in unknown_face_encodings:
-        # See if the face is a match for the known face(s)
+       
         matches = face_recognition.compare_faces(faces_encoded, face_encoding)
         name = "Unknown"
 
-        # use the known face with the smallest distance to the new face
+      
         face_distances = face_recognition.face_distance(faces_encoded, face_encoding)
         best_match_index = np.argmin(face_distances)
         if matches[best_match_index]:
@@ -67,16 +58,16 @@ def classify_face(im):
         face_names.append(name)
 
         for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Draw a box around the face
+          
             cv2.rectangle(img, (left-20, top-20), (right+20, bottom+20), (255, 0, 0), 2)
 
-            # Draw a label with a name below the face
+            
             cv2.rectangle(img, (left-20, bottom -15), (right+20, bottom+20), (255, 0, 0), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(img, name, (left -20, bottom + 15), font, 1.0, (255, 255, 255), 2)
 
 
-    # Display the resulting image
+   
     while True:
 
         cv2.imshow('Video', img)
