0759df2cb00d1a9a08463f96334f54ecd6cb1ee5
SAT <65762882+lkozina1309@users.noreply.github.com>
2021-10-27 14:54:21 +0200

    Add files via upload

---
+++ examples/image_face_detection.py
@@ -0,0 +1,15 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('/home/marija/OpenCV/haar_cascades.xml')
+img = cv2.imread('/home/marija/OpenCV/data/chelsea.jpeg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+faces = face_cascade.detectMultiScale(img, 1.1, 4)
+
+for (x, y , w ,h) in faces:
+	cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0 , 0), 3)
+	
+cv2.imshow('img', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
