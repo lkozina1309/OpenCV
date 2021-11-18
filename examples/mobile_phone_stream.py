af281eb20d99a550d2476aca1a3694b1c4b3d1b2
SAT <65762882+lkozina1309@users.noreply.github.com>
2021-11-18 12:34:09 +0100

    Update mobile_phone_stream.py

--- examples/mobile_phone_stream.py
+++ examples/mobile_phone_stream.py
@@ -1,6 +1,8 @@
+# Script mobile_phone_stream.py enables watching the internet stream from mobile phone or camera. You just have to url in the script where ("YOUR_URL") is written.
+
 import cv2
 
-cap = cv2.VideoCapture('http://192.168.5.16:8080/video')
+cap = cv2.VideoCapture('("YOUR_URL")/video')
 
 while(True):
 	ret, frame = cap.read()
