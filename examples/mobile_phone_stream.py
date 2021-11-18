788674442480a6256c2e96861d00eb07283bd4ef
SAT <65762882+lkozina1309@users.noreply.github.com>
2021-11-18 12:31:05 +0100

    Add files via upload

---
+++ examples/mobile_phone_stream.py
@@ -0,0 +1,13 @@
+import cv2
+
+cap = cv2.VideoCapture('http://192.168.5.16:8080/video')
+
+while(True):
+	ret, frame = cap.read()
+	cv2.imshow('stream', frame)   
+   
+	if cv2.waitKey(1) == ord('q'):
+		break
+		
+cap.release()
+cv2.destroyAllWindows()
