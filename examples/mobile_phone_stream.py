7b188931c168cdfecac1b8b24423142f43135f3d
SAT <65762882+lkozina1309@users.noreply.github.com>
2021-11-24 16:23:42 +0100

    Update mobile_phone_stream.py

--- examples/mobile_phone_stream.py
+++ examples/mobile_phone_stream.py
@@ -1,15 +1,23 @@
 # Script mobile_phone_stream.py enables watching the internet stream from mobile phone or camera. You just have to url in the script where ("YOUR_URL") is written.
 
 import cv2
+from mtcnn.mtcnn import MTCNN
 
-cap = cv2.VideoCapture('("YOUR_URL")/video')
+cap = cv2.VideoCapture('C:/Users/38595/Desktop/OpenCV/data/Megamind.avi')
 
-while(True):
-	ret, frame = cap.read()
-	cv2.imshow('stream', frame)   
-   
-	if cv2.waitKey(1) == ord('q'):
+while cap.isOpened():
+	_, img = cap.read()
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	detector = MTCNN()
+	faces = detector.detect_faces(img)
+	
+	for i in range (len(faces)):
+		x1, y1, width, height = faces[i]['box']
+		x2, y2 = x1 + width, y1 + height
+		cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+		
+	cv2.imshow('img', img)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 		
 cap.release()
-cv2.destroyAllWindows()
