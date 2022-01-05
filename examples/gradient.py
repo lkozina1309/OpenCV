fd3e90b4ab810c2d048d734b5735df0965e92cbd
Luka Kozina <lukakozina6@gmail.com>
2022-01-05 16:45:02 +0100

    Update gradient.py

--- examples/gradient.py
+++ examples/gradient.py
@@ -1,10 +1,18 @@
 import cv2
 
-img = cv2.imread('/home/marija/OpenCV/data/water_balloons.jpg')
+img = cv2.imread('/home/marija/OpenCV/data/mount.jpeg')
+img = cv2.resize(img, (1000,1000))
 
 lap = cv2.Laplacian(img, cv2.CV_64F, ksize=3)
+sobelX = cv2.Sobel(img, cv2.CV_64F, 1, 0)
+sobelY = cv2.Sobel(img, cv2.CV_64F, 0, 1)
+sobelCombined = cv2.bitwise_or(sobelX, sobelY)
 
 cv2.imshow('Laplacian', lap)
+cv2.imshow('sobelX', sobelX)
+cv2.imshow('sobelY', sobelY)
+cv2.imshow('sobelCombined', sobelCombined)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
