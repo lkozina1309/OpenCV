af3d8c5058e7aa5272e5f7ac129dfdc29699d492
Luka Kozina <lukakozina6@gmail.com>
2022-01-05 16:36:32 +0100

    Update gradient.py

--- examples/gradient.py
+++ examples/gradient.py
@@ -1,16 +1,10 @@
-# Script gradient.py shows gradients in an image. It uses image of a footballer Mason Mount so You need to download an image and provide the correct path to it.
-
-import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 
-img = cv2.imread('mount.jpeg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('/home/marija/OpenCV/data/water_balloons.jpg')
 
-titles = ['image']
-images = [img]
-for i in range(1):
-	plt.subplot(1,1,i+1), plt.imshow(images[i], 'gray')
-	plt.title(titles[i])
-	plt.xticks([]), plt.yticks([])
-	
-plt.show()
+lap = cv2.Laplacian(img, cv2.CV_64F, ksize=3)
+
+cv2.imshow('Laplacian', lap)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
