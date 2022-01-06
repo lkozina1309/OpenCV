7ed688d240c15408f8133cac1be90d876403607a
Luka Kozina <lukakozina6@gmail.com>
2022-01-06 09:35:01 +0100

    Update contours.py

--- examples/contours.py
+++ examples/contours.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('/home/marija/OpenCV/data/fruit.jpeg')
+img = cv2.imread('fruit.jpeg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(gray, 127, 255, 0)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
