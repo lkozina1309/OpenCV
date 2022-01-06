463aedd9fe37f998a7a478dd3714118b6b029587
Luka Kozina <lukakozina6@gmail.com>
2022-01-06 09:34:39 +0100

    Add files via upload

---
+++ examples/contours.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('/home/marija/OpenCV/data/fruit.jpeg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray, 127, 255, 0)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+
+cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
+cv2.drawContours(gray, contours, -1, (0, 255, 0), 3)
+
+cv2.imshow('Image', img)
+cv2.imshow('Gray', gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
