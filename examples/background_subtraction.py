7322cb9270165cff591354b8a088bd6b42885afe
Luka Kozina <lukakozina6@gmail.com>
2022-01-10 18:31:09 +0100

    Add files via upload

---
+++ examples/background_subtraction.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture('/home/marija/OpenCV/data/vtest.avi')
+#bg_sub = cv.bgsegm.createBackgroundSubtractorMOG()
+#bg_sub  = cv.createBackgroundSubtractorMOG2(detectShadows=True)
+bg_sub = cv.createBackgroundSubtractorKNN(detectShadows=True)
+
+while True:
+    ret, frame = cap.read()
+    if frame is None:
+        break
+    img = bg_sub.apply(frame)
+    cv.imshow('Background subtraction', img)
+
+    keyboard = cv.waitKey(30)
+    if keyboard == 'q' or keyboard == 27:
+        break
+cap.release()
+cv.destroyAllWindows()
