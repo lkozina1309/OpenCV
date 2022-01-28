7030ecd422b31bb5d46553de6479c0f9ad993dd6
Luka Kozina <lukakozina6@gmail.com>
2022-01-28 12:36:03 +0100

    Add files via upload

---
+++ examples/ocr.py
@@ -0,0 +1,17 @@
+import cv2 
+from PIL import Image
+import pytesseract
+
+
+def car_rec(image):
+    text = pytesseract.image_to_string(image)
+    return text
+
+img = cv2.imread("/home/marija/OpenCV/data/text.jpeg")
+
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+#img = cv2.medianBlur(img, 5)
+
+print(car_rec(img))
+
